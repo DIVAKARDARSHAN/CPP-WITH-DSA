5c89b4cbfdb963f4a4f13d3b2aec051ea41252c8
DIVAKARDARSHAN <divakardarshan28@gmail.com>
2024-06-02 03:48:32 +0530

    newfile

--- operators.cpp
+++ operators.cpp
@@ -1,4 +1,78 @@
+//........binary operators........
+// ------{'*','/','%','+','-'}-----
+  //#include <iostream>
+  //using namespace std;
+  //int main()
+ //{ 
+    //int a,b,c;
+    //cout<<"enter the number for a: ";
+    //cin>>a;
+    //cout<<"enter the number for b: ";
+    //cin>>b;
+    //cout<<"\nchoose any one operator for your numbers"<<"\n(1) '*'\n(2) '/'\n(3) '%'\n(4) '+'\n(5) '-'\n\n";
+    //char s;
+    //s= '*','/','%','+','-';
+    //cin>>s;
+   // if(s=='*')
+    //{
+        //c=a*b;
+        //cout<<"your answer is:"<<c;
+    //}
+    //else if(s=='/')
+    //{
+        //c=a/b;
+        //cout<<"your answer is:"<<c;
+    //}
+    //else if(s=='%')
+    //{
+        //c=a%b;
+        //cout<<"your answer is:"<<c;
+    //}
+    //else if(s=='+')
+     //{
+      //c=a+b;
+    // cout<<"your answer is:"<<c;
+     //}
+    //else if(s=='-')
+     //{
+        //c=a-b;
+        //cout<<"your answer is:"<<c;
+     //}
+    //else
+     //{
+        //cout<<"you choosed a wrong option";
+     //}
+//}
+
+//........Unary operations.........
+// -------{'++','--'}--------
+ //#include <iostream>
+ //using namespace std;
+ //int main()
+ //{
+    //int a,b,n;
+    //cout<<"enter the number: ";
+    //cin>>n;
+ //.......post increment.......
+    //a=n;
+    //b=a++;
+    //cout<<"\nthe value of b is: "<<b<<"\npost incremented value of a is: "<<a;
+ //.......pree increment.......
+    //a=n;
+    //b=++a;
+    //cout<<"\npree incremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;
+ //.......post dicremented.......
+    //a=n;
+    //b=a--;
+    //cout<<"\nthe value of b is: "<<b<<"\npost dicremented value of a is: "<<a;
+ //.......pree dicremented.......
+    //a=n;
+    //b=--a;
+    //cout<<"\npree dicremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;  
+//}
+
 //.........comparision operators............
+//-------{'<','>','==','<=','>=','!='}--------
   //#include <iostream>
   //using namespace std;
   //int main()
@@ -39,51 +113,4 @@
    //else
    //cout<<"\nno a is not equals to b";
 //}
-
-//........binary operators........
- 
-  #include <iostream>
-  using namespace std;
-  int main()
-{ 
-    int a,b,c,sign;
-    cout<<"enter the number for a: ";
-    cin>>a;
-    cout<<"enter the number for b: ";
-    cin>>b;
-    cout<<"\nchoose any one operator for your numbers \n(1) '*'\n(2) '/'\n(3) '%'\n(4) '+'\n(5) '-'\n\n";
-    cin>>sign;
-    sign= '*','/','%','+','-';
-      
-    if(sign='*')
-    {
-        c=a*b;
-        cout<<"your answer is:"<<c;
-    }
-    else if(sign='/')
-     {
-        c=a/b;
-        cout<<"your answer is:"<<c;
-     }
-    else if(sign='%')
-     {
-        c=a%b;
-        cout<<"your answer is:"<<c;
-     }
-    else if(sign='+')
-     {
-        c=a+b;
-        cout<<"your answer is:"<<c;
-     }
-    else if(sign='-')
-     {
-        c=a-b;
-        cout<<"your answer is:"<<c;
-     }
-    else
-     {
-        cout<<"you choosed a wrong option";
-     }
-     cout<<endl;
-}
 
