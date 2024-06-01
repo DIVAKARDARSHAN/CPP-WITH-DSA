fd803e148a64e33e9aa40bbe30eb09eb4959e48e
DIVAKARDARSHAN <divakardarshan28@gmail.com>
2024-06-02 04:01:38 +0530

    alignment

--- operators.cpp
+++ operators.cpp
@@ -1,116 +1,116 @@
 //........binary operators........
-// ------{'*','/','%','+','-'}-----
-  //#include <iostream>
-  //using namespace std;
-  //int main()
- //{ 
-    //int a,b,c;
-    //cout<<"enter the number for a: ";
-    //cin>>a;
-    //cout<<"enter the number for b: ";
-    //cin>>b;
-    //cout<<"\nchoose any one operator for your numbers"<<"\n(1) '*'\n(2) '/'\n(3) '%'\n(4) '+'\n(5) '-'\n\n";
-    //char s;
-    //s= '*','/','%','+','-';
-    //cin>>s;
-   // if(s=='*')
-    //{
-        //c=a*b;
-        //cout<<"your answer is:"<<c;
-    //}
+  // ------{'*','/','%','+','-'}-----
+    //#include <iostream>
+    //using namespace std;
+    //int main()
+  //{ 
+     //int a,b,c;
+     //cout<<"enter the number for a: ";
+     //cin>>a;
+     //cout<<"enter the number for b: ";
+     //cin>>b;
+     //cout<<"\nchoose any one operator for your numbers"<<"\n(1) '*'\n(2) '/'\n(3) '%'\n(4) '+'\n(5) '-'\n\n";
+     //char s;
+     //s= '*','/','%','+','-';
+     //cin>>s;
+    // if(s=='*')
+     //{
+         //c=a*b;
+         //cout<<"your answer is:"<<c;
+     //}
     //else if(s=='/')
-    //{
-        //c=a/b;
-        //cout<<"your answer is:"<<c;
-    //}
+     //{
+         //c=a/b;
+         //cout<<"your answer is:"<<c;
+     //}
     //else if(s=='%')
-    //{
-        //c=a%b;
-        //cout<<"your answer is:"<<c;
-    //}
+     //{
+         //c=a%b;
+         //cout<<"your answer is:"<<c;
+     //}
     //else if(s=='+')
      //{
-      //c=a+b;
-    // cout<<"your answer is:"<<c;
+         //c=a+b;
+         // cout<<"your answer is:"<<c;
      //}
     //else if(s=='-')
      //{
-        //c=a-b;
-        //cout<<"your answer is:"<<c;
+         //c=a-b;
+         //cout<<"your answer is:"<<c;
      //}
     //else
      //{
-        //cout<<"you choosed a wrong option";
+         //cout<<"you choosed a wrong option";
      //}
-//}
+  //}
 
 //........Unary operations.........
-// -------{'++','--'}--------
- //#include <iostream>
- //using namespace std;
- //int main()
- //{
-    //int a,b,n;
-    //cout<<"enter the number: ";
-    //cin>>n;
+  // -------{'++','--'}--------
+    //#include <iostream>
+    //using namespace std;
+    //int main()
+   //{
+       //int a,b,n;
+       //cout<<"enter the number: ";
+       //cin>>n;
  //.......post increment.......
-    //a=n;
-    //b=a++;
-    //cout<<"\nthe value of b is: "<<b<<"\npost incremented value of a is: "<<a;
+     //a=n;
+     //b=a++;
+     //cout<<"\nthe value of b is: "<<b<<"\npost incremented value of a is: "<<a;
  //.......pree increment.......
-    //a=n;
-    //b=++a;
-    //cout<<"\npree incremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;
+     //a=n;
+     //b=++a;
+     //cout<<"\npree incremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;
  //.......post dicremented.......
-    //a=n;
-    //b=a--;
-    //cout<<"\nthe value of b is: "<<b<<"\npost dicremented value of a is: "<<a;
+     //a=n;
+     //b=a--;
+     //cout<<"\nthe value of b is: "<<b<<"\npost dicremented value of a is: "<<a;
  //.......pree dicremented.......
-    //a=n;
-    //b=--a;
-    //cout<<"\npree dicremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;  
-//}
+     //a=n;
+     //b=--a;
+     //cout<<"\npree dicremented value of b is: "<<b<<"\nthe value of a becomes: "<<a;  
+   //}
 
 //.........comparision operators............
-//-------{'<','>','==','<=','>=','!='}--------
-  //#include <iostream>
-  //using namespace std;
-  //int main()
-//{
-    //int a,b;
-    //cout<<"enter the number for a: ";
-    //cin>>a;
-    //cout<<"enter the number for b: ";
-    //cin>>b;
+  //-------{'<','>','==','<=','>=','!='}--------
+    //#include <iostream>
+    //using namespace std;
+    //int main()
+ //{
+      //int a,b;
+      //cout<<"enter the number for a: ";
+      //cin>>a;
+      //cout<<"enter the number for b: ";
+      //cin>>b;
  //...checking grater then '>'...
-   //if(a>b)
-   //cout<<"\nyes a is grater then b";
-   //else
-   //cout<<"\nno b is grater then a";
+     //if(a>b)
+     //cout<<"\nyes a is grater then b";
+     //else
+     //cout<<"\nno b is grater then a";
  //...checking lesser then '<'...
-   //if(a<b)
-   //cout<<"\nyes a is lesser then b";
-   //else
-   //cout<<"\nno b is lesser then a";
+     //if(a<b)
+     //cout<<"\nyes a is lesser then b";
+     //else
+     //cout<<"\nno b is lesser then a";
  //...checking one equal to anotherone '=='...
-   //if(a==b)
-   //cout<<"\nyes a is equals to b";
-   //else
-   //cout<<"\nno a is not equals to b";
+     //if(a==b)
+     //cout<<"\nyes a is equals to b";
+     //else
+     //cout<<"\nno a is not equals to b";
  //...checking lesser then or equal to'<='...
-   //if(a<=b)
-   //cout<<"\nyes a is lesser then equals to b";
-   //else
-   //cout<<"\nno a is not lesser then equals to b";
+     //if(a<=b)
+     //cout<<"\nyes a is lesser then equals to b";
+     //else
+     //cout<<"\nno a is not lesser then equals to b";
  //...checking grater then or equal to'>='...
-   //if(a>=b)
-   //cout<<"\nyes a is grater then equals to b";
-   //else
-   //cout<<"\nno a is not grater then equals to b";
+     //if(a>=b)
+     //cout<<"\nyes a is grater then equals to b";
+     //else
+     //cout<<"\nno a is not grater then equals to b";
  //....checking one is not equal to anotherone '!='....
-   //if(a>=b)
-   //cout<<"\nyes a is not equals to b";
-   //else
-   //cout<<"\nno a is not equals to b";
-//}
+     //if(a>=b)
+     //cout<<"\nyes a is not equals to b";
+     //else
+     //cout<<"\nno a is not equals to b";
+   //}
 
