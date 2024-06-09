511a94ee19744fa28706dd626b3a192a4b668dc7
DIVAKARDARSHAN <divakardarshan28@gmail.com>
2024-06-09 20:03:28 +0530

    rename

---
+++ while&dowhile.cpp
@@ -0,0 +1,61 @@
+//..................grtting even/odd numbers...................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n,opp;     
+//     cout<<"enter the nth number: ";
+//     cin>>n;
+//     cout<<"choose any one of them \n 1.odd \n 2.even\n";
+//     cin>>opp;
+//    if(opp==1)
+//    {
+//     while(opp<=n)
+//     {
+//       cout<<opp<<" ";
+//       opp=opp+2;
+//     }
+//    }
+//    else if(opp==2)
+//    {
+//      while(opp<=n)
+//      {
+//        cout<<opp<<" ";
+//        opp=opp+2;
+//      }
+//    }
+//    else
+//     {
+//         cout<<"wrong opption";
+//     }
+// }
+
+//..................printing maths tables...................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n,i=1;
+//     cout<<"enter which table you want to print: ";
+//     cin>>n;
+//     while(i<=10)
+//     {
+//         cout<<n<<'*'<<i<<'='<<n*i<<endl;
+//         i=i+1;
+//     }
+// }
+    
+//..................printing sum of number in do-while..............
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1,sum=0,n;
+    cout<<"entir the number: ";
+    cin>>n;
+    do
+    {
+      cout<<sum+i;
+      i=i+1;
+    }while(i<=n);
+}
