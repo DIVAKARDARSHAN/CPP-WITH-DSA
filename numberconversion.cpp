8fe1aa4a26c835bae5454159289569de309d082c
DIVAKARDARSHAN <divakardarshan28@gmail.com>
2024-06-23 00:49:09 +0530

    new

---
+++ numberconversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cout<<"enter for a: ";
+    cin<<a;
+    cout<<"enter for b: ";
+    cin<<b;
+    cout<<"enter for c: ";
+    cin<<c;
+    cout<<"enter for d: ";
+    cin<<d;
+}
