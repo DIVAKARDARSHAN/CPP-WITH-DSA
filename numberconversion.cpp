1ad4d0114f16b0e2c1ca40491637ac6b96282c3d
DIVAKARDARSHAN <divakardarshan28@gmail.com>
2024-06-24 01:33:18 +0530

    newone

--- numberconversion.cpp
+++ numberconversion.cpp
@@ -1,14 +1,93 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a,b,c,d;
-    cout<<"enter for a: ";
-    cin<<a;
-    cout<<"enter for b: ";
-    cin<<b;
-    cout<<"enter for c: ";
-    cin<<c;
-    cout<<"enter for d: ";
-    cin<<d;
-}
+// //...............decimal to binary..........
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//  int num,rem,ans=0,mul=1;
+//  cout<<"enter the number:";
+//  cin>>num;
+//  while(num>0)
+//  {
+//     rem=num%2;
+//     num/=2;
+//     ans+=rem*mul;
+//      mul*=10;
+//  }
+//  cout<<ans;
+// }
+  
+//...............binary to decimal..........
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//  int num,rem,ans=0,mul=1;
+//  cout<<"enter the binary number: ";
+//  cin>>num;
+//  while(num>0)
+//  {
+//     rem=num%10;
+//     num/=10;
+//     ans+=rem*mul;
+//      mul*=2;
+//  }
+//  cout<<ans;
+// }
+
+//...............decimal to octat.............
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int num,rem,ans=0,mul=1,temp;
+//   cout<<"enter the decimal number: ";
+//   cin>>num;
+//   while(num>0)
+//   {
+//     rem=num%8;
+//     num=num/8;
+//     ans=rem*mul+ans;
+//     mul*=10;
+//   }
+//   cout<<ans;
+// }
+
+//...............octal to decimal.............
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int num,rem,ans=0,mul=1,temp;
+//   cout<<"enter the decimal number: ";
+//   cin>>num;
+//   while(num>0)
+//   {
+//     rem=num%10;
+//     num=num/10;
+//     ans=rem*mul+ans;
+//     mul*=8;
+//   }
+//   cout<<ans;
+// }
+
+//...............binary to octal.................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int num,ans=0,rem,mul=1,temp;
+//     cout<<"enter the binary number: ";
+//     cin>>num;
+//     while(num!=0)
+//     {
+//         rem=num%10;
+//         num=num/10;
+//         ans=rem*mul+ans;
+//         mul=mul*2;
+//          rem=num%8;
+//         num=num/8;
+//         ans=rem*mul+ans;
+//         mul=mul*10;
+//     }
+//     cout<<ans;
+// }
